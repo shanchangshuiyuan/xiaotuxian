64b39911a3f19b24273c8bf30df9deae6ce447e4
xd_jiangwenting <2920485521@qq.com>
2024-05-13 18:44:58 +0800

    feat:2024/5/13-分类组件的封装

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,18 +1,24 @@
 <template>
+  <!-- 自定义导航栏 -->
   <CustomNavbar></CustomNavbar>
+  <!-- 自定义导航栏 -->
   <XtxSwiper :list="bannerList"></XtxSwiper>
+  <!-- 分类面板 -->
+  <CategoryPanel></CategoryPanel>
   <view class="index">index</view>
 </template>
 
 <script setup lang="ts">
 import { getHomeBannerAPI } from '@/services/home'
-import CustomNavbar from './component/CustomNavbar.vue'
 import { onLoad } from '@dcloudio/uni-app'
 import { ref } from 'vue'
 //以Xtx开头的组件可以自动导入，后面在使用就不导入了
 // import XtxSwiper from '@/components/XtxSwiper.vue'
 import type { BannerItem } from '@/types/home'
+import CustomNavbar from './component/CustomNavbar.vue'
+import CategoryPanel from './component/CategoryPanel.vue'
 
+//轮播图数据获取
 const bannerList = ref<BannerItem[]>([])
 const getHomeBannerData = async () => {
   const res = await getHomeBannerAPI()
@@ -25,5 +31,7 @@ onLoad(() => {
 </script>
 
 <style lang="scss">
-//
+page {
+  background-color: #f7f7f7;
+}
 </style>
