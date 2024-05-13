458e3089be434c68c81360c0bc0ecea31e1b97d4
xd_jiangwenting <2920485521@qq.com>
2024-05-13 10:50:04 +0800

    feat：2024/5/13-轮播图指示点

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,9 +1,12 @@
 <template>
   <CustomNavbar></CustomNavbar>
+  <XtxSwiper></XtxSwiper>
   <view class="index">index</view>
 </template>
 <script setup lang="ts">
 import CustomNavbar from './component/CustomNavbar.vue'
+//以Xtx开头的组件可以自动导入，后面在使用就不导入了
+// import XtxSwiper from '@/components/XtxSwiper.vue'
 </script>
 
 <style lang="scss">
